1dc77182d7a29b12adc3cbd726cf0acbfead7127
josephdsbr <josephdsbr@gmail.com>
2020-11-28 15:23:13 -0300

    configuracoes de lint e typescript

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Header>
+        <title>Ola mundo</title>
+      </Header>
+      <h1>Olá Mundo</h1>
+    </div>
+  )
+}
+
+export default Home
